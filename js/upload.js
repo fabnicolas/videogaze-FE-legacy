59f54b6cd350202f7c96fdbfca0a7877145f9824
Finalgalaxy <finalgalaxy@gmail.com>
2018-03-15 01:07:52 +0100

    Preview support.

--- js/upload.js
+++ js/upload.js
@@ -5,7 +5,7 @@
 	}
 
 
-	// file drag hover
+	// Triggered when mouse hovers the rectangle while file is dragged.
 	function eventhandler_mousehover_dragging_file(e) {
 		e.stopPropagation();
 		e.preventDefault();
@@ -13,30 +13,39 @@
 	}
 
 
-	// file selection
-	function eventhandler_mousedrop_fileselected(e) {
+	// Triggered when mouse hovers the rectangle and file is dropped (Like uploading it).
+	function eventhandler_fileselected(e) {
 		console.log("FileSelectHandler");
 		console.log(e);
 
-		// cancel event and hover styling
-		FileDragHover(e);
+		// Cancel hovering events.
+		eventhandler_mousehover_dragging_file(e);
 
-		// fetch FileList object
+		// Fetch FileList object
 		var files = e.target.files || e.dataTransfer.files;
 
-		// process all File objects
-		for (var i = 0, f; f = files[i]; i++) {
+		// Process all File objects (without multiple enabled there's only one).
+		for(var i = 0, f; f = files[i]; i++) {
 			ParseFile(f);
 		}
 
 	}
 
 
-	// output file information
+	// Output file information
 	function ParseFile(file) {
 		console.log(file);
 
 		console.log(window.URL.createObjectURL(file));
+
+		if(file.type.indexOf("video")>-1){
+			  var video_element = document.getElementById("video-preview");
+			  video_element.src = URL.createObjectURL(file);
+			  video_element.type = file.type;
+			  video_element.style.display = "block";
+			  video_element.play();
+			  video_element.onplaying = function(){console.log("ok");setTimeout(function(){video_element.pause()},5000);};
+		}
 
 		Output(
 			"<p>File information: <strong>" + file.name +
@@ -47,31 +56,33 @@
 	}
 
 
-	// initialize
-	function Init() {
+	// Initialize the uploader.
+	function uploader_init() {
 		var fileselect = document.getElementById("fileselect");
 		var filedrag = document.getElementById("filedrag");
 		var submitbutton = document.getElementById("submitbutton");
+		var video_element = document.getElementById("video-preview");
 
 		// File selected.
-		fileselect.addEventListener("change", eventhandler_mousedrop_fileselected, false);
+		fileselect.addEventListener("change", eventhandler_fileselected, false);
 
-		// XHR2 available
+		// XHR2 available?
 		if((new XMLHttpRequest()).upload){
 			// File drop handlers
 			filedrag.addEventListener("dragover", eventhandler_mousehover_dragging_file, false);	// Mouse inside it
 			filedrag.addEventListener("dragleave", eventhandler_mousehover_dragging_file, false);	// Mouse outside it
-			filedrag.addEventListener("drop", eventhandler_mousedrop_fileselected, false);	// File dropped
+			filedrag.addEventListener("drop", eventhandler_fileselected, false);	// File dropped
 			filedrag.style.display = "block";
 
 			// Remove submit button.
 			submitbutton.style.display = "none";
+			video_element.style.display = "none";
 		}
 
 	}
 
 	// If file management is allowed on the browser
 	if (window.File && window.FileList && window.FileReader) {
-		Init();
+		uploader_init();
 	}
 })();

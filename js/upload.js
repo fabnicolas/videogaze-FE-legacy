bb55c7ddebcff921166841d1499fccbe911b4e01
Finalgalaxy <finalgalaxy@gmail.com>
2018-03-16 20:11:21 +0100

    Upgraded chunking capabilities, fixes.

--- js/upload.js
+++ js/upload.js
@@ -13,6 +13,39 @@ var ChunkUploader = (function() {
         });
     }
 
+    var composeChunksInBackend = function(key, chunk_number, max_chunks){
+        if(chunk_number==0){
+            console.log("Composing file with key '"+key+"' in backend. Number of chunks: "+max_chunks+".");
+            console.log("Waiting for server response...");
+            chunk_number++;
+        }
+        
+        var fd = new FormData();
+		fd.append("key",key);
+        fd.append("chunk_number",chunk_number);
+        fd.append("max_chunks",max_chunks);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://finalgalaxy.unaux.com/videogaze-BE/compose_chunks.php');
+        xhr.onloadend = function(e) {
+            if(xhr.status!=200){
+                console.log("FAILED to compose chunk #"+chunk_number+". Status = "+xhr.status+".");
+                console.log("Recomposing chunk #"+chunk_number+"...");
+                composeChunksInBackend(key, chunk_number, max_chunks);
+            }else{
+                console.log("Composed chunk #"+chunk_number+" in backend. Status = "+xhr.status);
+                if(chunk_number===max_chunks){
+                    console.log("Done! Transferred and composed: "+max_chunks+" chunks.");
+                }else{
+                    chunk_number++;
+                    console.log("Attempting to compose in backend chunk #"+chunk_number+"...");
+                    composeChunksInBackend(key, chunk_number, max_chunks);
+                }
+            }
+        };
+        xhr.send(fd);
+    }
+
     var uploadFile = function(file) {
         var BYTES_PER_CHUNK, SIZE, NUM_CHUNKS, start, end;
         BYTES_PER_CHUNK = parseInt(1048576, 10);
@@ -26,37 +59,60 @@ var ChunkUploader = (function() {
 		var key = uuidv4();
 
         while(start < SIZE) {
-            console.log("Simulation: " + key + "," + chunk_id + "," + NUM_CHUNKS);
-            uploadChunk(file.slice(start, end), key, chunk_id, NUM_CHUNKS);
+            //console.log("Simulation: " + key + "," + chunk_id + "," + NUM_CHUNKS);
+            uploadChunk(file.slice(start, end), key, chunk_id, NUM_CHUNKS, composeChunksInBackend);
             start = end;
             end = start + BYTES_PER_CHUNK;
             chunk_id++;
         }
     }
 
-    var uploadChunk = function(chunkFile, key, chunk_number, max_chunks) {
+    var chunks_uploaded=0;
+    var uploadChunk = function(chunkFile, key, chunk_number, max_chunks, callback_enduploadchunks=null, call_again=true) {
 		var fd = new FormData();
 		fd.append("chunk_file",chunkFile);
 		fd.append("key",key);
 		fd.append("chunk_number",chunk_number);
 		fd.append("max_chunks",max_chunks);
-		
+        
+        console.log("Sending chunk temporary file: "+key+"_part"+chunk_number+".tmp...");
+        //console.log(chunkFile);
         var xhr = new XMLHttpRequest();
-		xhr.open('POST', 'http://finalgalaxy.unaux.com/videogaze-BE/handle_file_upload.php', true);
+		xhr.open('POST', 'http://finalgalaxy.unaux.com/videogaze-BE/handler_chunks.php', true);
         xhr.upload.onprogress = function(e) {
             if(e.lengthComputable) {
                 var perc = Math.round((e.loaded / e.total) * 100);
-                perc = parseFloat(perc) + '%';
-                console.log(perc);
+                if(parseFloat(perc)===100){
+                    chunks_uploaded++;
+                    console.log("A chunk got transferred. Chunks uploaded: "+chunks_uploaded);
+                }
             }
         };
         xhr.onloadend = function(e) {
             uploaders.pop();
-            if(!uploaders.length) console.log("All sent!");
+            if(!uploaders.length){
+                console.log("All "+chunks_uploaded+" chunks got transferred successfully!");
+                chunks_uploaded=0;  // Reset
+                if(callback_enduploadchunks!=null) callback_enduploadchunks(key, 0, max_chunks);
+            }
         };
+        // Listen for response status.
+        xhr.onreadystatechange = function(){ 
+            // If request fails, attempt to send chunk again just one time.
+            if(xhr.readyState == 4 && xhr.status != 200) {
+                if(call_again) uploadChunk(chunkFile, key, chunk_number, max_chunks, composeChunksInBackend, !call_again);
+                else            console.log("Error "+xhr.status);
+            }
+        };
+        // Listen for errors.
+        xhr.onerror = function () { 
+            console.log("On error called");
+            if(call_again) uploadChunk(chunkFile, key, chunk_number, max_chunks, composeChunksInBackend, !call_again);
+            else            console.log("Error on XHR.");
+        }; 
         uploaders.push(xhr);
         xhr.send(fd);
-	};
+    };
 	
 	return{
 		uploadFile: uploadFile,
@@ -131,10 +187,11 @@ var ChunkUploader = (function() {
 
     /* NEW */
     function UploadFile(file) {
+        //console.log(file);
         ChunkUploader.uploadFile(file);
     }
 
-    /* DEPRECATED
+    /*
     function UploadFile(file) {
         console.log("triggered file upload");
         var fd = new FormData();
